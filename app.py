077b8d763ccc25d0474cd408dccf69e3909b042f
Yexi <blaizh@gmail.com>
2022-03-27 13:26:47 +0800

    refactored get-district-price api to flatten response before sending to frontend

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from dis import dis
 import os
 import json
 
-from flask import Flask ,jsonify
+from flask import Flask, request, jsonify
 from pyairtable import Table
 
 api_key = os.environ["AIRTABLE_API_KEY"]
@@ -10,32 +11,30 @@ app = Flask(__name__)
 
 base_id = "app509gADynVIIgpa"
 
-district_14 = Table(api_key, base_id, "tblAFuETAyGakpAtR")
-district_15 = Table(api_key, base_id, "tblMTEg8eUNngnBSS")
-district_16 = Table(api_key, base_id, "tblAGBm5Uukc1J4GR")
-district_bedok = Table(api_key, base_id, "tblgN0Rs9zWp2AApr")
-district_yishun = Table(api_key, base_id, "tbluw6Y017VvxCouK")
+district_14 = "tblAFuETAyGakpAtR"
+district_15 = "tblMTEg8eUNngnBSS"
+district_16 = "tblAGBm5Uukc1J4GR"
+district_bedok = "tblgN0Rs9zWp2AApr"
+district_yishun = "tbluw6Y017VvxCouK"
 
-@app.route('/district-14')
-def get_district_14():
-	return jsonify(district_14.all())
-
-@app.route('/district-15')
-def get_district_15():
-	return jsonify(district_15.all())
-
-@app.route('/district-16')
-def get_district_16():
-	return jsonify(district_16.all())
-
-@app.route('/district-bedok')
-def get_district_bedok():
-	return jsonify(district_bedok.all())
-
-@app.route('/district-yishun')
-def get_district_yishun():
-	return jsonify(district_yishun.all())
-
+@app.route('/get-district-price', methods=['GET'])
+def getAirTablePriceList():
+    # List to store price list
+    priceList = []
+    
+    table_id = request.args.get('table_id')
+    
+    response = Table(api_key, base_id, table_id)
+    airTableData = response.all()
+    
+    for i in airTableData:
+        tableRow = i['fields']
+        tableRow = i['fields']
+        tableRow['id'] = i['id']
+        tableRow['createdTime'] = i['createdTime']
+        priceList.append(tableRow)
+    
+    return jsonify(priceList) 
+    
 if __name__ == "__main__":
 	app.run()
- 
