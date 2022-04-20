9d0170fd1f45ecb665d8c55b69ce100974ad87de
yuan-yexi <yuan-yexi@pop-os.localdomain>
2022-04-20 21:57:10 +0800

    added logic to sort hdb price list

--- app.py
+++ app.py
@@ -27,31 +27,45 @@ def getAirTablePriceList():
     response = Table(api_key, base_id, table_id)
     airTableData = response.all()
     
-    for i in airTableData:
-        tableRow = i['fields']
-        tableRow = i['fields']
-        tableRow['id'] = i['id']
-        tableRow['createdTime'] = i['createdTime']
-        priceList.append(tableRow)
-        
-    for i in airTableData:
-        tableRow = i['fields']
-    tableRow = i['fields']
-    tableRow['id'] = i['id']
-    tableRow['createdTime'] = i['createdTime']
-    priceList.append(tableRow)
+    if table_id == district_14 or table_id == district_15 or table_id == district_15:
+        for i in airTableData:
+            tableRow = i['fields']
+            tableRow = i['fields']
+            tableRow['id'] = i['id']
+            tableRow['createdTime'] = i['createdTime']
+            priceList.append(tableRow)
 
-    # create dataframe to format date and sorty by formated_date by descending
-    dataframe = pd.DataFrame(priceList)
-    dataframe['formated_date'] = pd.to_datetime(dataframe['Date of Sale'], format='%b-%y')
-    dataframe.sort_values(by='formated_date', ascending=False, inplace=True)
-    dataframe.reset_index(inplace=True)
-    dataframe['No.'] = dataframe.index + 1
-    
-    # convert dataframe back to json
-    fromat_price_list =  dataframe.to_json(orient="records")
-    
-    return fromat_price_list
-    
+        # create dataframe to format date and sorty by formatted_date by descending
+        dataframe = pd.DataFrame(priceList)
+        dataframe['formatted_date'] = pd.to_datetime(dataframe['Date of Sale'], format='%b-%y')
+        dataframe.sort_values(by='formatted_date', ascending=False, inplace=True)
+        dataframe.reset_index(inplace=True)
+        dataframe['No.'] = dataframe.index + 1
+        
+        # convert dataframe back to json
+        fromat_price_list =  dataframe.to_json(orient="records")
+        
+        return fromat_price_list
+
+    elif table_id == district_bedok or table_id == district_yishun:
+        for i in airTableData:
+            tableRow = i['fields']
+            tableRow = i['fields']
+            tableRow['id'] = i['id']
+            tableRow['createdTime'] = i['createdTime']
+            priceList.append(tableRow)
+
+        # create dataframe to format date and sorty by formatted_date by descending
+        dataframe = pd.DataFrame(priceList)
+        dataframe['formatted_date'] = pd.to_datetime(dataframe['Resale Registration Date'], format='%b %Y')
+        dataframe.sort_values(by='formatted_date', ascending=False, inplace=True)
+        dataframe.reset_index(inplace=True)
+        dataframe['No.'] = dataframe.index + 1
+        
+        # convert dataframe back to json
+        fromat_price_list =  dataframe.to_json(orient="records")
+        
+        return fromat_price_list
+
 if __name__ == "__main__":
 	app.run()
